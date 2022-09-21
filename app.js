a7c85e2db00c51ae0fbe7256ed0b4eac52eca5f7
Cody Palmer <codyryanpalmer@gmail.com>
2022-09-21 15:40:41 -0500

    Updates to styling and card layout

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ document.querySelector('.deleteButton').addEventListener('click',(e)=>{
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
         document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("form").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
     } else {
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("form").style.visibility = "visible";
-        document.querySelector("body").style.visibility = "visible";
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }, 1000)
     }
 };
 
